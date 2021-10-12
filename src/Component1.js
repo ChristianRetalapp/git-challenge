0a0221f7e51018ffba9978cf324704fe07611e06
ChristianRetalapp <christian.cajiao@retalapp.co>
2021-10-12 18:56:51 -0500

    background color and icon

--- src/Component1.js
+++ src/Component1.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+ 
 export const Component1 = (props) => {
-  return (
-    <div>
-      <button className="button">{props.name}</button>
-    </div>
-  );
+ return (
+   <div>
+     <button className="button">{props.name}</button>
+   </div>
+ );
 };
