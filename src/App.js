148af2501ada0d85b6c490482ed2615b9336b2ad
Kentruri <53955778+Kentruri@users.noreply.github.com>
2021-10-21 15:31:28 -0500

    background color changes and button

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
          <input type="text" />
          <label>apellido</label>
          <input type="text" />
+        <Component1 name={"test"}/>
          <Component1 name={"submit"} />
        </form>
      </div>
