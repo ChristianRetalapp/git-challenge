e57148cb1de9b85cc256227530bdcd709a1d6be3
jonathan <jonathang2812@hotmail.com>
2021-10-25 10:19:58 -0500

    color-email

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ function App() {
          <input type="text" />
          <label>apellido</label>
          <input type="text" />
+         <label>email</label>
+         <input type="text" />
          <Component1 name={"submit"} />
        </form>
      </div>
